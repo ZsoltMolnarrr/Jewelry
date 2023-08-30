41a40f6ec7fd00983a78f8a1fb54d91c283536fa
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-08-30 21:35:48 +0200

    Add Fabric mod

---
+++ src/main/java/net/jewelry/jewelry/Jewelry.java
@@ -0,0 +1,13 @@
+package net.jewelry.jewelry;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Jewelry implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/jewelry/jewelry/client/JewelryClient.java
@@ -0,0 +1,13 @@
+package net.jewelry.jewelry.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JewelryClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
