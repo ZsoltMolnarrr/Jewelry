1368af6c8f8194e5c680bd090b55ca27f74f92c5
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-19 12:32:24 +0200

    Fabric multi slot compat

--- fabric/src/main/java/net/jewelry/fabric/compat/CompatFeatures.java
+++ fabric/src/main/java/net/jewelry/fabric/compat/CompatFeatures.java
@@ -1,9 +1,60 @@
 package net.jewelry.fabric.compat;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.jewelry.JewelryMod;
 import net.jewelry.fabric.compat.trinkets.TrinketsCompat;
+import net.tiny_config.ConfigManager;
 
 public class CompatFeatures {
+    private static final ConfigManager<FabricCompatConfig> config = new ConfigManager<>
+            ("fabric_compatibility", new FabricCompatConfig())
+            .builder()
+            .setDirectory(JewelryMod.ID)
+            .sanitize(true)
+            .build();
+    private static boolean configLoaded = false;
+    private static FabricCompatConfig safeConfig() {
+        if (!configLoaded) {
+            config.refresh();
+            configLoaded = true;
+        }
+        return config.value;
+    }
+
     public static void init() {
-        TrinketsCompat.init();
+        initSlotCompat();
+    }
+
+    private static void initSlotCompat() {
+        var preferred = safeConfig().preferred_slot_mod_id;
+        if ("trinkets".equals(preferred)) {
+            if (initTrinkets()) {
+                return;
+            }
+        } else if ("accessories".equals(preferred)) {
+            if (initAccessories()) {
+                return;
+            }
+        }
+        if (initTrinkets()) {
+            return;
+        }
+        initAccessories();
+    }
+
+    private static boolean initTrinkets() {
+        if (FabricLoader.getInstance().isModLoaded("trinkets")) {
+            TrinketsCompat.init();
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean initAccessories() {
+        if (FabricLoader.getInstance().isModLoaded("accessories")) {
+            CompatFeatures.init();
+            return true;
+        }
+        return false;
     }
 }
--- fabric/src/main/java/net/jewelry/fabric/compat/trinkets/TrinketsCompat.java
+++ fabric/src/main/java/net/jewelry/fabric/compat/trinkets/TrinketsCompat.java
@@ -2,21 +2,15 @@ package net.jewelry.fabric.compat.trinkets;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.jewelry.items.Factory;
-import net.jewelry.items.JewelryItem;
-import net.minecraft.component.type.AttributeModifiersComponent;
-import net.minecraft.item.Item;
 
 public class TrinketsCompat {
     public static void init() {
         if (FabricLoader.getInstance().isModLoaded("trinkets")) {
-            Factory.Holder.factory = new Factory() {
-                @Override
-                public JewelryItem create(Item.Settings settings, AttributeModifiersComponent attributes, String lore, String slot) {
-                    var item = new JewelryTrinketItem(settings, lore);
-                    // Passing attriubtes here instead Item.Settings, because Trinkets ignores `AttributeModifiersComponent`
-                    item.setConfigurableModifiers(attributes);
-                    return item;
-                }
+            Factory.Holder.factory = (settings, attributes, lore, slot) -> {
+                var item = new JewelryTrinketItem(settings, lore);
+                // Passing attriubtes here instead Item.Settings, because Trinkets ignores `AttributeModifiersComponent`
+                item.setConfigurableModifiers(attributes);
+                return item;
             };
         }
     }
