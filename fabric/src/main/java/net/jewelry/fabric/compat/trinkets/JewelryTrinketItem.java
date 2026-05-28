d903a3bb574f508b36290d0dfa6aa6fb259843e3
vao211 <vaolu211@gmail.com>
2026-05-28 07:16:50 +0700

    Fix: Resolve Trinket equip rollbacks and attribute stacking bugs, compat with ExtraSpellAttribute

--- fabric/src/main/java/net/jewelry/fabric/compat/trinkets/JewelryTrinketItem.java
+++ fabric/src/main/java/net/jewelry/fabric/compat/trinkets/JewelryTrinketItem.java
@@ -18,14 +18,21 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
+import java.util.Map;
 
 public class JewelryTrinketItem extends TrinketItem implements JewelryItem {
+    // PHỤC HỒI LẠI BIẾN CỦA TÁC GIẢ
     private AttributeModifiersComponent customAttributes = AttributeModifiersComponent.builder().build();
     private final String lore;
 
     public JewelryTrinketItem(Settings settings, String lore) {
         super(settings);
         this.lore = lore;
+    }
+
+    // PHỤC HỒI LẠI HÀM CỦA TÁC GIẢ ĐỂ KHÔNG BỊ LỖI BÊN TRINKETSHELPER
+    public void setConfigurableModifiers(AttributeModifiersComponent component) {
+        this.customAttributes = component;
     }
 
     @Override
@@ -36,21 +43,40 @@ public class JewelryTrinketItem extends TrinketItem implements JewelryItem {
         }
     }
 
+    @Override
     public Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, Identifier slotIdentifier) {
-        var defaultModifiers = super.getModifiers(stack, slot, entity, slotIdentifier);
-        Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> mutableModifiers = ArrayListMultimap.create(defaultModifiers);
-        String itemName = net.minecraft.registry.Registries.ITEM.getId(stack.getItem()).getPath();
+        Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> uniqueModifiers = ArrayListMultimap.create();
 
-        for (var entry : this.customAttributes.modifiers()) {
-            Identifier uniqueModId = Identifier.of(slotIdentifier.getNamespace(),
-                    slotIdentifier.getPath() + "_" + itemName + "_" + entry.modifier().id().getPath());
-            mutableModifiers.put(entry.attribute(),
-                    new EntityAttributeModifier(uniqueModId, entry.modifier().value(), entry.modifier().operation()));
+        try {
+            Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> defaultModifiers = super.getModifiers(stack, slot, entity, slotIdentifier);
+            String itemName = net.minecraft.registry.Registries.ITEM.getId(stack.getItem()).getPath();
+
+            for (Map.Entry<RegistryEntry<EntityAttribute>, EntityAttributeModifier> entry : defaultModifiers.entries()) {
+                EntityAttributeModifier modifier = entry.getValue();
+                String attributeName = entry.getKey().value().getTranslationKey().replace("attribute.name.", "");
+
+                String rawPath = slotIdentifier.getPath() + "_" + itemName + "_base_" + attributeName + "_" + modifier.operation().name();
+                String safePath = rawPath.toLowerCase(java.util.Locale.ROOT).replaceAll("[^a-z0-9/._-]", "_");
+
+                uniqueModifiers.put(entry.getKey(), new EntityAttributeModifier(Identifier.of(slotIdentifier.getNamespace(), safePath), modifier.value(), modifier.operation()));
+            }
+            for (AttributeModifiersComponent.Entry entry : this.customAttributes.modifiers()) {
+                EntityAttributeModifier modifier = entry.modifier();
+                String attributeName = entry.attribute().value().getTranslationKey().replace("attribute.name.", "");
+
+                String rawPath = slotIdentifier.getPath() + "_" + itemName + "_" + attributeName + "_" + modifier.operation().name();
+                String safePath = rawPath.toLowerCase(java.util.Locale.ROOT).replaceAll("[^a-z0-9/._-]", "_");
+
+                Identifier uniqueModId = Identifier.of(slotIdentifier.getNamespace(), safePath);
+
+                uniqueModifiers.put(entry.attribute(),
+                        new EntityAttributeModifier(uniqueModId, modifier.value(), modifier.operation()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return mutableModifiers;
-    }
-    public void setConfigurableModifiers(AttributeModifiersComponent component) {
-        this.customAttributes = component;
+
+        return uniqueModifiers;
     }
 
     @Override
